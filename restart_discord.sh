1c785c37fa66b6c4f7bb464d5fd1a789c5d491c7
Schoperation <cschop1@gmail.com>
2023-01-07 14:36:28 -0500

    Adjust timing for moving discord window

--- restart_discord.sh
+++ restart_discord.sh
@@ -7,12 +7,15 @@ pkill Discord
 
 # Clear tmp folder of discord-infested stuff
 rm -r /tmp/.org.chromium.Chromium.*
+# flatpak edition command
 #nohup /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=discord com.discordapp.Discord &>/dev/null &
+
+# normal
 nohup discord &>/dev/null &
-sleep 5s
+sleep 7s
 
 # manipulate discord window
-wmctrl -r Discord -e 0,1920,0,1920,1060
+wmctrl -r Friends -e 0,1920,0,1920,1060
 #sleep 0.5s
 #wmctrl -r Discord -b toggle,maximized_vert,maximized_horz
 
