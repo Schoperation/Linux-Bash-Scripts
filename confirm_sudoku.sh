6133da069639305f1fd6dd7e607dbc8672359697
Schoperation <cschop1@gmail.com>
2025-01-03 01:41:23 -0500

    confirm_sudoku: Add col and square validation

--- confirm_sudoku.sh
+++ confirm_sudoku.sh
@@ -11,7 +11,45 @@ ValidateRow () {
         if [ ${foundNumbers[$(($num - 1))]} -eq 0 ]; then
             foundNumbers[$(($num - 1))]=$num
         else
-            echo "Invalid $num at ($(($startingCell / 9 + 1)),$(($cell + 1)))"
+            echo "Invalid $num at ($(($startingCell / 9 + 1)),$(($cell + 1))), while validating row"
+            return 1
+        fi
+
+        #echo "${foundNumbers[@]}"
+    done
+}
+
+ValidateCol () {
+    sudoku=$1
+    startingCell=$2
+
+    foundNumbers=(0 0 0 0 0 0 0 0 0)
+    for cell in {0..8}
+    do
+        num=${sudoku[$(($startingCell + $cell * 9))]}
+        if [ ${foundNumbers[$(($num - 1))]} -eq 0 ]; then
+            foundNumbers[$(($num - 1))]=$num
+        else
+            echo "Invalid $num at ($(($cell + 1)),$(($startingCell + 1))), while validating col"
+            return 1
+        fi
+
+        #echo "${foundNumbers[@]}"
+    done
+}
+
+ValidateSquare () {
+    sudoku=$1
+    startingCell=$2
+
+    foundNumbers=(0 0 0 0 0 0 0 0 0)
+    for cell in 0 1 2 9 10 11 18 19 20;
+    do
+        num=${sudoku[$(($startingCell + $cell))]}
+        if [ ${foundNumbers[$(($num - 1))]} -eq 0 ]; then
+            foundNumbers[$(($num - 1))]=$num
+        else
+            echo "Invalid $num at ($(($startingCell / 9 + 1)),$(($cell / 9 + 1))), while validating square"
             return 1
         fi
 
@@ -20,7 +58,7 @@ ValidateRow () {
 }
 
 # Start of Main
-sudoku=()
+sudoku=(1 2 3 4 5 6 7 8 9 2 3 4 5 6 7 8 9 1 3 4 5 6 7 8 9 1 2 4 5 6 7 8 9 1 2 3 5 6 7 8 9 1 2 3 4 6 7 8 9 1 2 3 4 5 7 8 9 1 2 3 4 5 6 8 9 1 2 3 4 5 6 7 9 1 2 3 4 5 6 7 8)
 
 echo "Enter in your Sudoku grid one row at a time."
 echo "E.g. 123456789"
@@ -47,5 +85,15 @@ done
 for row in {0..72..9}
 do
     ValidateRow $sudoku $(($row))
+done
+
+for col in {0..8}
+do
+    ValidateCol $sudoku $(($col))
+done
+
+for sq in 0 3 6 27 30 33 54 57 60;
+do
+    ValidateSquare $sudoku $(($sq))
 done
 
